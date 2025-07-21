d7e1311a80b182881cd966b302fd72f734d39506
mtakram <m.tariq.dev@gmail.com>
2025-07-21 21:57:29 +0500

    variables commited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144555;
+let accountEmail = "tariq@google.com";
+var accountPassword = "12345";
+let accountCity = "Lahore";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope
+*/
