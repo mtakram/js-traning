28f9a28a51867864e75e9a7e8151e9b0114bb781
mtakram <m.tariq.dev@gmail.com>
2025-07-21 22:28:45 +0500

    data types committed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version
+ 
+let accountNumber = 12345;
+let accountName = "Tariq";
+let accountLogin = false;
+let accountValue = null;
+let accountState;
+
+// Primitive Data Types
+// number => 2 to power 53
+// bigint => For larger numbers
+// string => "Tariq"
+// boolean => true/false
+// null => standalone value (object)
+// undefined => undefined
+// symbol => unique
+
+console.table([accountNumber, accountName, accountLogin, accountValue, accountState]);
+
+console.log(typeof accountName);
+
+console.log(typeof accountValue);
+
+console.log(typeof accountState);
